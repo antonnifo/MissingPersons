75df6c2b7e7f353a18d5c8451ea89cf7e95ce438
antonnifo <antonnifo@live.com>
2023-08-23 09:26:46 +0300

    include human app urls

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('persons.urls', namespace='persons')),
+    path('account/', include('human.urls')),
+    path('social-auth/',
+            include('social_django.urls', namespace='social')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
