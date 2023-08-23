f17bb3fa6fcef397b69056c652f0ca9fc978417e
antonnifo <antonnifo@live.com>
2023-08-23 16:02:38 +0300

    fx persons typo

--- persons/urls.py
+++ persons/urls.py
@@ -6,8 +6,7 @@ app_name = 'persons'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<int:year>/<int:month>/<int:day>/<str:pperson>/',
+    path('<int:year>/<int:month>/<int:day>/<str:person>/',
          views.person_detail,
          name='person_detail'),
-                  
 ]
