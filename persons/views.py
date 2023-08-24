d48b334def150d4b87e9d5ef7c98d9976d38cebf
antonnifo <antonnifo@live.com>
2023-08-24 12:26:39 +0300

    paginate object list

--- persons/views.py
+++ persons/views.py
@@ -2,19 +2,32 @@ from django.shortcuts import get_object_or_404, redirect, render
 from hitcount.utils import get_hitcount_model
 from hitcount.views import HitCountMixin
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 from .models import Comment, Person
 
 
-def home(request):
-    persons = Person.objects.all()
-    
-    context = {'persons': persons}
-    return render(request, 'person/index.html', context)
+def home(request):  
+
+    OBJECT_LIST =  Person.objects.all()
+    paginator = Paginator(OBJECT_LIST, 8)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)        
+    return render(request, 'person/index.html', {
+                                                   'posts' : posts,
+                                                   'page' : page,
+       })    
 
 
 def person_detail(request, year, month, day, person):
     person = get_object_or_404(Person,
-           
                              last_name=person,
                              reported_at__year=year,
                              reported_at__month=month,
